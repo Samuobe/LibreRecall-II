b7b761daeb5a19aaaa3c8c01ed34075cbe5eca80
Samuobe <samuobe@ik.me>
2025-09-14 11:11:11 +0200

    Fix paths in desktop entry and update PKGBUILD for LibreRecall installation

---
+++ remove_from_here.sh
@@ -0,0 +1,8 @@
+sudo rm -rf /usr/bin/LibreRecall
+
+
+sudo rm /home/samuobe/.config/autostart/LibreRecall_daemon.desktop
+
+sudo rm /home/samuobe/.local/share/applications/LibreRecall.desktop
+
+sudo rm /usr/share/pixmaps/LibreRecall.png
